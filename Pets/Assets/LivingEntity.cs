99d4fa4136c2d62be47ae56bee9b63d8140e20e6
Ankit <43947543+AnkitNarsingani@users.noreply.github.com>
2019-01-25 22:38:10 +0530

    Ronak scene added

---
+++ Pets/Assets/LivingEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LivingEntity : MonoBehaviour
+{
+    public void Feed()
+    {
+
+    }
+
+    public void Shit()
+    {
+
+    }
+
+    public abstract void Action();
+
+}
