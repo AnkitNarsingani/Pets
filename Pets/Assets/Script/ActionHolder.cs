f5b2ab07751216e26aae34c2ef595761b58b52b1
RonakFabain <ronakmanfabiaan@gmail.com>
2019-01-26 15:08:43 +0530

    Scripts/Assets

---
+++ Pets/Assets/Script/ActionHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Animal", menuName = "Animal")]
+public class ActionHolder : ScriptableObject
+{
+   
+    public string animal;
+    public int timer;
+    public bool isDone;
+    public Sprite Image;
+    
+    public enum ActionState{ Action,Feed,Shit,Done}
+    public ActionState currState;
+}
---
+++ Pets/Assets/Script/Display.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour {
+
+    public ActionHolder animal;
+    public Image img;
+    void Start ()
+    {
+        img.sprite = animal.Image;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
